7717bd4976d9367643b240e78c8b046e63ac061e
sdsd08013 <sd08013+git@gmail.com>
2022-05-19 15:38:46 +0900

    added clip test

---
+++ test/clip_list_test.dart
@@ -0,0 +1,44 @@
+import 'package:clipit/clip.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('new text make shouldUpdate returns true', () {
+    final list = ClipList(value: [
+      Clip(
+          id: 1,
+          text: 'text1',
+          count: 1,
+          isSelected: true,
+          createdAt: DateTime.now(),
+          updatedAt: DateTime.now())
+    ]);
+
+    expect(list.shouldUpdate('text2'), true);
+  });
+  test('when 59mins passed existing text make shouldUpdate returns false', () {
+    final list = ClipList(value: [
+      Clip(
+          id: 1,
+          text: 'text1',
+          count: 1,
+          isSelected: true,
+          createdAt: DateTime.now(),
+          updatedAt: DateTime.now().subtract(const Duration(minutes: 59)))
+    ]);
+
+    expect(list.shouldUpdate('text1'), false);
+  });
+  test('when 2hours passed existing text make shouldUpdate returns false', () {
+    final list = ClipList(value: [
+      Clip(
+          id: 1,
+          text: 'text1',
+          count: 1,
+          isSelected: true,
+          createdAt: DateTime.now(),
+          updatedAt: DateTime.now().subtract(const Duration(hours: 2))),
+    ]);
+
+    expect(list.shouldUpdate('text1'), true);
+  });
+}
