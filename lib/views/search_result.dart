1f29b1e3daa344eeac319bb8f480d269538eb5b1
sdsd08013 <sd08013+git@gmail.com>
2022-06-25 18:41:43 +0900

    bugfix search result layout

--- lib/views/search_result.dart
+++ lib/views/search_result.dart
@@ -9,28 +9,31 @@ class SearchResultView extends StatelessWidget {
   SearchResultView({required this.results});
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
+    return ListView.builder(
         shrinkWrap: true,
-        itemBuilder: (context, parentIndex) =>
-            Text(results[parentIndex].listTitle),
-        separatorBuilder: (context, parentIndex) => ListView.separated(
-            shrinkWrap: true,
-            itemBuilder: (context, childIndex) => Container(
-                height: 75,
-                padding: const EdgeInsets.all(8),
-                color: results[parentIndex].value[childIndex].isSelected
-                    ? side2ndBackgroundSelect
-                    : side2ndBackground,
-                child: RichText(
-                  text: TextSpan(
-                    text: results[parentIndex].value[childIndex].plainText,
-                    style: const TextStyle(
-                        color: textColor, fontFamily: "RictyDiminished"),
-                  ),
-                )),
-            separatorBuilder: (context, childIndex) =>
-                const Divider(color: dividerColor, height: 0.5),
-            itemCount: results[parentIndex].value.length),
+        itemBuilder: (context, parentIndex) => Column(children: [
+              Text(results[parentIndex].listTitle),
+              ListView.separated(
+                  shrinkWrap: true,
+                  physics: const ClampingScrollPhysics(),
+                  itemBuilder: (context, childIndex) => Container(
+                      height: 75,
+                      padding: const EdgeInsets.all(8),
+                      color: results[parentIndex].value[childIndex].isSelected
+                          ? side2ndBackgroundSelect
+                          : side2ndBackground,
+                      child: RichText(
+                        text: TextSpan(
+                          text:
+                              results[parentIndex].value[childIndex].plainText,
+                          style: const TextStyle(
+                              color: textColor, fontFamily: "RictyDiminished"),
+                        ),
+                      )),
+                  separatorBuilder: (context, childIndex) =>
+                      const Divider(color: dividerColor, height: 0.5),
+                  itemCount: results[parentIndex].value.length)
+            ]),
         itemCount: results.length);
   }
 }
